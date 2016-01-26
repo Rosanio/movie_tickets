53c8722f0a66fd8fb21dbb449c1d065d4ceb2765
Anna Becker and Matt Rosanio <anna.r.becker@gmail.com>
2016-01-26 11:02:28 -0800

    add jQuery functionality

--- js/scripts.js
+++ js/scripts.js
@@ -19,12 +19,32 @@ Ticket.prototype.price = function() {
   if (this.age <= 10 || this.age >= 65) {
     price -= 2;
   }
-  if(this.species === 'manitee') {
-    price = 0;
+  for (var j = 0; j < this.species.length; j++) {
+    if(this.species[j] === "manitee") {
+      price = 0;
+    }
   }
   return price;
 }
 
-$(document).ready(function() {
+$(function() {
+  $("form#questions").submit(function(event) {
+    var movie = $("select#movie").val();
+    var time = parseInt($("select#time").val());
+    var age = parseInt($("input#age").val());
+    var species = [];
+    $.each($('input[name="iden"]:checked'), function() {
+      species.push($(this).val());
+    });
+    var newTicket = new Ticket(movie, time, age, species);
+    var price = newTicket.price();
+    console.log('it works');
+    console.log(species);
+
+    $(".ticketPrice").text(price);
+    $("#price").show();
+
+    event.preventDefault();
+  });
 
 });
