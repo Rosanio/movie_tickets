98c62d2287a8ef71d5a7ec57f7134eabda5d3fb9
Anna Becker and Matt Rosanio <anna.r.becker@gmail.com>
2016-01-26 10:03:46 -0800

    add price method to ticket objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
-function Ticket(movie, time, age) {
+function Ticket(movie, time, age, species) {
   this.movie = movie;
   this.time = time;
   this.age = age;
+  this.species = species;
+}
+
+Ticket.prototype.price = function() {
+  var price = 9;
+  var theaterMovies = ["Spotlight", "Sisters", "Carol", "Joy", "The Revenant"];
+  for (var i = 0; i < theaterMovies.length; i++) {
+    if (this.movie === theaterMovies[i]) {
+      price += 1;
+    }
+  }
+  if (this.time <= 4) {
+    price -= 1;
+  }
+  if (this.age <= 10 || this.age >= 65) {
+    price -= 2;
+  }
+  if(this.species === 'manitee') {
+    price = 0;
+  }
+  return price;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,14 @@
 describe("Ticket",function() {
   it("will return a ticket object with entered values",function() {
-    var testTicket = new Ticket("Inglorious Bastards","9:30",91);
+    var testTicket = new Ticket("Inglorious Bastards",9,91,"manitee");
     expect(testTicket.movie).to.equal("Inglorious Bastards");
-    expect(testTicket.time).to.equal("9:30");
+    expect(testTicket.time).to.equal(9);
     expect(testTicket.age).to.equal(91);
+    expect(testTicket.species).to.equal("manitee");
+  });
+
+  it("will calculate a price based on user information", function () {
+    var testTicket = new Ticket ("Inglorious Bastards",9,91,"human");
+    expect(testTicket.price()).to.equal(7);
   });
 });
